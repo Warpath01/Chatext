a1fb32c125e334ce4a251a1dcc9468e3a55b0750
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-03-01 09:48:23 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -6,35 +6,44 @@ import Chat from "./pages/Chat";
 import ProtectedRoute from "./components/Protected/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 import { useAuthStore } from "./store/auth.store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import AuthLayout from "./components/auth-layout/AuthLayout";
 
 function App() {
-  const { checkAuth, isAuthenticated } = useAuthStore();
+  const { checkAuth, authUser, isLoading, setIsLoading } = useAuthStore();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     checkAuth();
   }, []);
 
+  if (isLoading) return <div>Loading...</div>;
   return (
     <div>
       <Routes>
         <Route
-          path="/signup"
-          element={!isAuthenticated ? <Signup /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!isAuthenticated ? <Login /> : <Navigate to="/" />}
-        />
-
-        {/* this routes are proteced */}
-        <Route
           path="/"
-          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+          element={!authUser ? <AuthLayout /> : <Navigate to="/home" />}
+        >
+          <Route
+            path="signup"
+            element={!authUser ? <Signup /> : <Navigate to="/home" />}
+          />
+          <Route
+            path="login"
+            element={!authUser ? <Login /> : <Navigate to="/home" />}
+          />
+        </Route>
+
+        {/* These routes are protected */}
+        <Route
+          path="/home"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
         />
         <Route
           path="/chat"
-          element={isAuthenticated ? <Chat /> : <Navigate to="/login" />}
+          element={authUser ? <Chat /> : <Navigate to="/login" />}
         />
       </Routes>
     </div>
