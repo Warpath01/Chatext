7d3e9fc00a54e36b2148c980ac0888e5e5a7098f
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-03-02 18:58:09 +0800

    Update chat.store.js

--- frontend/src/store/chat.store.js
+++ frontend/src/store/chat.store.js
@@ -10,103 +10,119 @@ export const useChatStore = create((set, get) => ({
     selectedUser: null,
     selectWorldChat: false,
 
+
     getUsers: async () => {
+        const token = useAuthStore.getState().accessToken;
         try {
             const res = await fetch(`${BASE_URL}/api/posts/users`, {
                 method: "GET",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
-                credentials: "include"
+                headers: { "Authorization": `Bearer ${token}` },
+                credentials: "include",
             });
-              if (!res.ok) throw new Error("No users!");
             const data = await res.json();
             set({ users: data });
         } catch (error) {
-            console.log("No users!");
+            console.log("Error in getUsers store", error.message)
+            res.status(500).json({ message: "Internal Server Error." })
         }
     },
 
     getPublicMessages: async () => {
+        const token = useAuthStore.getState().accessToken;
         try {
             const res = await fetch(`${BASE_URL}/api/messages/all`, {
                 method: "GET",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
-                credentials: "include"
-            });
-if (!res.ok) throw new Error("No Public Message!");
+                headers: { "Authorization": `Bearer ${token}` },
+                credentials: "include",
+            })
             const data = await res.json();
             set({ messages: data });
         } catch (error) {
-            console.log("No Public Message!");
+            console.log("Error in getPublicMessages store", error.message)
+            res.status(500).json({ message: "Internal Server Error." })
         }
     },
 
+
     sendPublicMessages: async (message) => {
+        const token = useAuthStore.getState().accessToken;
         try {
-            const { messages } = get();
+
+            const { messages } = get()
 
             const res = await fetch(`${BASE_URL}/api/messages/send/all`, {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
+                headers: { "Authorization": `Bearer ${token}` },
                 body: JSON.stringify(message),
-                credentials: "include"
-            });
-              if (!res.ok) throw new Error("Error in sending message!");
+                credentials: "include",
+            })
             const data = await res.json();
             set({ messages: [...messages, data] });
         } catch (error) {
-            console.log("Error in sending message!");
+            console.log("Error in sendPublicMessages store", error.message)
+            res.status(500).json({ message: "Internal Server Error." })
         }
     },
 
+
+
     getPersonalMessage: async (userId) => {
+        const token = useAuthStore.getState().accessToken;
         try {
             const res = await fetch(`${BASE_URL}/api/messages/${userId}`, {
                 method: "GET",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
-                credentials: "include"
-            });
-              if (!res.ok) throw new Error("No personal message!");
+                headers: { "Authorization": `Bearer ${token}` },
+                credentials: "include",
+            })
             const data = await res.json();
             set({ messages: data });
         } catch (error) {
-            console.log("No personal message!");
+            console.log("Error in getPersonalMessage store", error.message)
+            res.status(500).json({ message: "Internal Server Error." })
         }
     },
 
     sendPersonalMessage: async (message) => {
+        const token = useAuthStore.getState().accessToken;
         try {
-            const { selectedUser, messages } = get();
+            const { selectedUser, messages } = get()
 
             const res = await fetch(`${BASE_URL}/api/messages/send/${selectedUser._id}`, {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
+                headers: { "Authorization": `Bearer ${token}` },
                 body: JSON.stringify(message),
-                credentials: "include"
-            });
-               if (!res.ok) throw new Error("Error in sending message!");
+                credentials: "include",
+            })
             const data = await res.json();
             set({ messages: [...messages, data] });
         } catch (error) {
-            console.log("Error in sending message!");
+            console.log("Error in sendPersonalMessage store", error.message)
+            res.status(500).json({ message: "Internal Server Error." })
         }
+
     },
 
-    // Realtime application - Personal
+    // realtime application -------- Personal
     receiveMessage: async () => {
         const { selectedUser } = get();
         if (!selectedUser) return;
 
-        const socket = useAuthStore.getState().getSocket();
+        const socket = useAuthStore.getState().getSocket(); // Get the socket instance safely
         if (!socket) return console.warn("Socket is not initialized yet.");
 
         socket.on("newMessage", (newMessage) => {
@@ -114,17 +130,18 @@ if (!res.ok) throw new Error("No Public Message!");
             set({ messages: [...get().messages, newMessage] });
         });
     },
-
     unReceiveMessage: async () => {
-        const socket = useAuthStore.getState().getSocket();
+
+        const socket = useAuthStore.getState().getSocket(); // Get the socket instance safely
         if (!socket) return console.warn("Socket is not initialized yet.");
         socket.off("newMessage");
     },
 
-    // Realtime application - Public
+
+    // realtime applictaion -------- Public
     receivePublicMessages: async () => {
         const { selectWorldChat } = get();
-        if (selectWorldChat) {
+        if (selectWorldChat === true) {
             const socket = useAuthStore.getState().getSocket();
             if (!socket) return console.warn("Socket is not initialized yet.");
 
@@ -133,13 +150,14 @@ if (!res.ok) throw new Error("No Public Message!");
             });
         }
     },
-
     unReceivePublicMessages: async () => {
-        const socket = useAuthStore.getState().getSocket();
+
+        const socket = useAuthStore.getState().getSocket(); // Get the socket instance safely
         if (!socket) return console.warn("Socket is not initialized yet.");
         socket.off("newPublicMessage");
     },
 
+
     setSelectedUser: (selectedUser) => set({ selectedUser }),
-    setSelectWorldChat: (selectWorldChat) => set({ selectWorldChat })
-}));
+    setSelectWorldChat: (selectWorldChat) => set({ selectWorldChat }),
+}))
