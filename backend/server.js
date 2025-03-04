b70d8f5c16ae024f819b7204eae75f5a4e486ae6
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-03-04 15:34:26 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
     });
 
     app.use('/profile-pics', express.static(path.join(__dirname, 'uploaded', 'profilepics')));
