0f4dd5cceb7f1ab6480a2300c69ed7d0905479b5
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-03-04 14:41:53 +0800

    Update chat.store.js

--- frontend/src/store/chat.store.js
+++ frontend/src/store/chat.store.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { useAuth } from "../context/AuthContext";
 import { useAuthStore } from "./auth.store";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:7000" : "https://chatext-elfm.onrender.com";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:7000" : "chatext-server-n859tr5cq-warps-projects-5dfd9456.vercel.app";
 
 export const useChatStore = create((set, get) => ({
     messages: [],
