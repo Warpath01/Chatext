86e39c940a6219b5b4d76f66b5bfefd6241075d8
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-03-04 15:19:09 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,16 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://chatext-client.onrender.com");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
